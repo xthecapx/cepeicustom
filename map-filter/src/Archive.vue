f9d53ca07bebf896a95190008a26d79fae198951
The Cap <xthecapx@gmail.com>
2017-04-21 14:59:12 -0500

    Added filters

--- map-filter/src/Archive.vue
+++ map-filter/src/Archive.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="archive">
+    <h3 class="section-title">ARCHIVO</h3>
     <div class="filter">
+      <div class="label">
+        FILTRAR POR:
+      </div>
       <select name="pais" id="pais" v-model="tagSelected" @change="updateTags">
         <option v-for="tag in tags" :value="tag.value">{{ tag.name }}</option>
       </select>
+      <select name="tema" id="tema" v-model="temaSelected" @change="updateTemas">
+        <option v-for="tema in temas" :value="tema.value">{{ tema.name }}</option>
+      </select>
+      <div class="main-img">
+        <img :src="imgSelected" alt="">
+      </div>
     </div>
     <ul class="content-wrapper">
       <li v-for="(post, key) in posts" class="post">
@@ -12,10 +22,10 @@
         </div>
         <h3>{{post.title}}</h3>
         <div class="content" v-html="post.content"></div>
-        <div class="clearfix" v-if="key%4 == 0"></div>
       </li>
     </ul>
     <paginate
+      v-if="pagination.totalpages > 1"
       :page-count="pagination.totalpages"
       :click-handler="showMore"
       :prev-text="'<'"
@@ -35,9 +45,11 @@ export default {
         totalpages: 0
       },
       consult: {
-        default: "&per_page=8&type[]=eventos_cepei&type[]=gobernanzas&type[]=bibliotecas&type[]=datos&_embed",
-        page: "page=1",
+        default: "&type[]=eventos_cepei&type[]=gobernanzas&type[]=bibliotecas&type[]=datos&_embed",
+        page: "&page=1",
+        perPage: "&per_page=8"
       },
+      embed: "&_embed",
       url: "",
       queryParams: "",
       filter: {
@@ -50,7 +62,42 @@ export default {
       tags: [
         {name: "colombia", value: "218"}
       ],
-      tagSelected: ""
+      paisesIMG: {
+        "218": "http://a3d.db3.myftpupload.com/wp-content/uploads/2017/04/mapa-eventos-bogota.png"
+      },
+      temas: [
+        {
+          name: "Todos",
+          value: "&type[]=eventos_cepei&type[]=gobernanzas&type[]=bibliotecas&type[]=datos"
+        },
+        {
+          name: "Eventos",
+          value: "&type[]=eventos_cepei"
+        },
+        {
+          name: "Gobernanza",
+          value: "&type[]=gobernanzas"
+        },
+        {
+          name: "Multimedia",
+          value: "&type[]=bibliotecas"
+        },
+        {
+          name: "Datos",
+          value: "&type[]=datos"
+        }
+      ],
+      categorias: [
+        {
+          name: "Eventos",
+          value: "?categories=325"
+        }
+      ],
+      temaSelected: "",
+      imgSelected: "",
+      tagSelected: "",
+      filteredTag: "",
+      filteredTema: ""
     }
   },
   methods: {
@@ -68,7 +115,7 @@ export default {
       a.href = window.location.href
 
       if (a.hostname == "localhost") {
-        this.url = "/api/wp-json/wp/v2/multiple-post-type?categories=325"
+        this.url = "/api/wp-json/wp/v2/multiple-post-type"
         this.headers = {
           total: "x-wp-total",
           totalPages: "x-wp-totalpages"
@@ -77,19 +124,37 @@ export default {
       }
 
       if (this.getParameterByName("post_type") == "eventos_cepei") {
-        this.url = "/wp-json/wp/v2/multiple-post-type?categories=325"
+        this.url = "/wp-json/wp/v2/multiple-post-type"
       }
         
     },
     showMore(pageNum) {
-      let url = ""
-
-      this.queryParams = "&page=" + pageNum + "&" + this.consult.default
-      url = this.url + this.queryParams
-      this.updateData(url)
+      this.updateData(this.url + 
+                      this.categorias[0].value + 
+                      "&page=" + pageNum +
+                      this.consult.perPage +
+                      this.temas[0].value +
+                      this.embed)
     },
     updateTags() {
-      this.updateData(this.url + "&tags=" + this.tagSelected + "&" + this.consult.default)
+      this.filteredTag = "&tags=" + this.tagSelected
+
+      this.updateData(this.url + 
+                      this.categorias[0].value + 
+                      this.consult.page +
+                      this.consult.perPage +
+                      this.filteredTag + 
+                      this.temaSelected +
+                      this.embed)
+      this.imgSelected = this.paisesIMG[this.tagSelected]
+    },
+    updateTemas() {
+      this.updateData(this.url + 
+                      this.categorias[0].value + 
+                      this.consult.page +
+                      this.consult.perPage +
+                      this.temaSelected +
+                      this.embed)
     },
     updateData(url) {
       this.$http.get(url)
@@ -106,36 +171,24 @@ export default {
                   this.posts.push(data)
               }
 
-          }, response => {
-              // error callback
-              console.log("e")
-          });
-    }
-  },
-  created() {
-      let url = ""
-      this.defineUrl()
-
-      url = this.url + this.consult.default
-
-      this.$http.get(url)
-          .then(response => {
-              for (let i = 0, l = response.data.length; i < l; i++) {
-                  let data = {
-                      title: response.data[i].title.rendered,
-                      bg: response.data[i]._embedded["wp:featuredmedia"] && response.data[i]._embedded["wp:featuredmedia"][0].source_url,
-                      content: response.data[i].excerpt.rendered
-                  }
-
-                  this.posts.push(data)
-              }
-
               this.pagination['total'] = parseInt(response.headers.map[this.headers.total][0])
               this.pagination['totalpages'] = parseInt(response.headers.map[this.headers.totalPages][0])
           }, response => {
               // error callback
               console.log("e")
           });
+    },
+  },
+  created() {
+      this.defineUrl()
+      this.imgSelected = this.paisesIMG["218"]
+      this.temaSelected = this.temas[0].value
+      this.updateData(this.url + 
+                      this.categorias[0].value + 
+                      this.consult.page +
+                      this.consult.perPage +
+                      this.temaSelected +
+                      this.embed)
   }
 }
 </script>
